1fc2b3442746e725be2c0e17930d0e2b6aeab456
phucbm <hiphucbui@gmail.com>
2026-02-17 08:27:23 +0700

    feat: add runtime edge for cloudflare

--- app/docs/route.ts
+++ app/docs/route.ts
@@ -1,5 +1,7 @@
 import { NextResponse } from 'next/server';
 
+export const runtime = 'edge';
+
 export async function GET() {
   const apiUrl = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:3002';
 
