fff79932fe92a452c85810e45783f1b0ec5391ee
phucbm <hiphucbui@gmail.com>
2026-03-07 17:02:13 +0700

    fix: build error

--- app/app/my-cards/[id]/page.tsx
+++
@@ -1,9 +0,0 @@
-import CardDetailClient from './card-detail-client';
-
-export function generateStaticParams() {
-  return [];
-}
-
-export default function CardDetailPage() {
-  return <CardDetailClient />;
-}
