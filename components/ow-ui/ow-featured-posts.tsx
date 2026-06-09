0e14c27195ee6e592f5dedc68f2263eec72290bf
phucbm <hiphucbui@gmail.com>
2026-06-09 18:04:29 +0700

    refactor(ow-featured-posts): update resolvePosts import path

--- components/ow-ui/ow-featured-posts.tsx
+++ components/ow-ui/ow-featured-posts.tsx
@@ -1,6 +1,6 @@
 import Link from 'next/link';
 import type {Post} from '@/lib/mdx';
-import {resolvePosts} from '@/lib/mdx';
+import {resolvePosts} from '@/lib/posts-utils';
 import {ROUTES} from '@/lib/routes';
 import {OwPostCategoryDate} from './ow-post-category-date';
 import {OwButton} from "@/components/ow-ui/ow-button";
